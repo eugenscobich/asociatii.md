99e015a99c0afbed6e3509707c36157efe69331b
Eugen Scobich <eugen.scobich@mail.ru>
2015-07-12 14:48:15 +0300

    save

--- sources/asociatii.md-wedding-portlet/src/main/webapp/js/lodging_gmaps.js
+++ sources/asociatii.md-wedding-portlet/src/main/webapp/js/lodging_gmaps.js
@@ -200,13 +200,13 @@ function mapRedefine(){
   myWidth = window.innerWidth;
   if(myWidth <=767){
     var currCenter = new google.maps.LatLng(47.035609, 28.869928);
-    map.setZoom(16);
+    map.setZoom(15);
   }else if(myWidth = 768){
     var currCenter = new google.maps.LatLng(47.035609, 28.869928);
-    map.setZoom(16);
+    map.setZoom(15);
   }else{
     var currCenter = center;
-    map.setZoom(16);
+    map.setZoom(15);
   }
 
   map.setCenter(currCenter);
